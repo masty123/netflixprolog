95821cfab67a2d82da7597e05e19015a7acc64e5
masty123 <pwnimatorz@hotmail.com>
2020-03-25 14:07:05 +0700

    first commit

---
+++ netflix_rule.pl
@@ -0,0 +1,300 @@
+%member:
+%theeruth   borisuth        5910545701
+%phanuwatch luangpradit     5910545779
+%patiphan   srisook         5910546422
+%tetach     rattanavikran   6110545554
+%punyawee   srithongkerd    6110545571
+
+% topic:_Watching_Netflix
+
+%_source_reference:
+%_Netflix.com
+%_https://www.wired.co.uk/article/best-shows-netflix
+%_https://www.radiotimes.com/news/on-demand/2020-02-11/netflix-best-movies/?
+
+%_competency_Question:
+
+% Kid-friendly media?
+% Classic_media?
+% Latest movies in Netflix?
+% An anime or not?
+% Similar media with the same age restriction?
+% Director that directed a movie that this specific actor appear
+% An co-stars of a specific director in a specific movie.  
+% Director that direct movie for mature audiance
+% actors in classic a media.
+% co-star in a movie?
+% Specific mature  movies with specific type that release in a specific year.
+% Director that used to direct a classic media.
+   
+
+%fact
+movie(deadpool).
+movie(titanic).
+movie(the_End_of_Evangelion).
+movie(jumanji).
+movie(marriage_story).
+movie(frozen).
+movie(charlie_and_the_chocolate_factory).
+movie(uncut_Gems).
+movie(the_thing).
+movie(back_to_the_future).
+movie(ready_player_One).
+
+series(sex_Education).
+series(locke_and_Key).
+series(prison_break).
+series(stranger_things).
+series(the_Witcher).
+series(pokemon_sun_and_moon).
+series(ben10).
+
+
+releaseYear(uncut_Gems, 2020).
+releaseYear(deadpool, 2016).
+releaseYear(jumanji, 2017).
+releaseYear(frozen, 2013).
+releaseYear(marriage_story, 2019).
+releaseYear(the_End_of_Evangelion, 1997).
+releaseYear(titanic, 1997).
+releaseYear(charlie_and_the_chocolate_factory, 2005).
+releaseYear(the_thing, 1982).
+releaseYear(back_to_the_future, 1985).
+releaseYear(ready_player_One, 2018).
+releaseYear(sex_Education, 2020).
+releaseYear(locke_and_Key, 2020).
+releaseYear(prison_break, 2017).
+releaseYear(stranger_things, 2019).
+releaseYear(the_Witcher, 2019).
+releaseYear(pokemon_sun_and_moon, 2019).
+releaseYear(ben10, 2005).
+
+matage(deadpool, 18).
+matage(jumanji, 13).
+matage(frozen, 7).
+matage(marriage_story, 16).
+matage(the_End_of_Evangelion, 18).
+matage(titanic, 16).
+matage(charlie_and_the_chocolate_factory, 7).
+matage(sex_Education, 18).
+matage(locke_and_Key, 18).
+matage(prison_break, 18).
+matage(stranger_things, 16).
+matage(the_Witcher, 18).
+matage(the_thing, 18).
+matage(pokemon_sun_and_moon, 7).
+matage(uncut_Gems, 18).
+matage(back_to_the_future, 10).
+matage(ready_player_One, 12).
+matage(ben10, 6).
+
+type(deadpool, action).
+type(deadpool, comedy).
+type(jumanji, comedy).
+type(jumanji, family).
+type(frozen, family).
+type(marriage_story, drama).
+type(the_End_of_Evangelion, action).
+type(the_End_of_Evangelion, fantasy).
+type(the_End_of_Evangelion, animation).
+type(the_End_of_Evangelion, sci-fI).
+type(titanic, romance).
+type(titanic, drama).
+type(sex_Education, teen).
+type(sex_Education, drama).
+type(sex_Education, comedy).
+type(locke_and_Key, fantasy).
+type(locke_and_Key, teen).
+type(locke_and_Key, drama).
+type(prison_break, crime).
+type(prison_break, drama).
+type(stranger_things, thriller).
+type(stranger_things, teen).
+type(the_Witcher, fantasy).
+type(the_Witcher, drama).
+type(pokemon_sun_and_moon, animation).
+type(pokemon_sun_and_moon, action).
+type(pokemon_sun_and_moon, family).
+type(charlie_and_the_chocolate_factory, family).
+type(charlie_and_the_chocolate_factory, comedy).
+type(the_thing, horror).
+type(back_to_the_future, fantasy).
+type(ready_player_One, fantasy).
+type(uncut_Gems, action).
+type(ben10, teen).
+type(ben10, animation).
+
+isfrom(frozen, usa).
+isfrom(marriage_story, usa).
+isfrom(the_End_of_Evangelion, japan).
+isfrom(titanic, usa).
+isfrom(sex_Education, usa).
+isfrom(locke_and_Key, usa).
+isfrom(prison_break, usa).
+isfrom(stranger_thing, usa).
+isfrom(the_Witcher, usa).
+isfrom(pokemon_sun_and_moon, japan).
+isfrom(charlie_and_the_chocolate_factory, usa).
+isfrom(the_thing, usa).
+isfrom(back_to_the_future, usa).
+isfrom(ready_player_One, usa).
+isfrom(deadpool, usa).
+isfrom(jumanji, usa).
+isfrom(uncut_Gems, usa).
+isfrom(ben10, usa).
+
+director(frozen, jennifer_lee).
+director(marriage_story, noah_baumbach).
+director(the_End_of_Evangelion, hideaki_anno).
+director(titanic, jame_cameron).
+director(sex_Education, ben_taylor).
+director(locke_and_Key, michael_morris).
+director(prison_break, paul_sheuring).
+director(stranger_thing, shawn_leavy).
+director(the_Witcher, lars_mikkelsen).
+director(pokemon_sun_and_moon, shigeru_omori).
+director(charlie_and_the_chocolate_factory, tim_burton).
+director(the_thing, john_carpenter).
+director(back_to_the_future, robert_zemekis).
+director(ready_player_One, steven_spelberg).
+director(deadpool, tim_miller).
+director(jumanji, jake_kasdan).
+director(uncut_Gems, josh_safdie).
+director(ben10, matt_youngberg).
+
+
+actor(frozen, idina_menzel, elsa).
+actor(frozen, kristen_bell, anna).
+actor(frozen, josh_gad, olaf).
+actor(marriage_story, adam_driver, charlie).
+actor(marriage_story, scarlett_johansson, nicole).
+actor(the_End_of_Evangelion, megumi_ogata, shiniji).
+actor(the_End_of_Evangelion, megumi_hayashibara, rei_ayanami).
+actor(the_End_of_Evangelion, yuko_miyamura, asuka_langley).
+actor(titanic, kate_winslet, rose_dewitt).
+actor(titanic, leonardo_decaprio, jack_dawson).
+actor(sex_Education,asa_butterfield, otis_milburn).
+actor(sex_Education, emma_mackey, maeve_wiley).
+actor(sex_Education, ncuta_gatwa, erica_effiong).
+actor(locke_and_Key, jackson_robert_scott, bode_locke).
+actor(locke_and_Key, corner_jessup, tyler_locke).
+actor(prison_break, emilia_jones,kinsey_locke).
+actor(stranger_thing, millie_bobby_brown, eleven).
+actor(stranger_thing, finn_wolfhard, mike_wheeler).
+actor(stranger_thing, gaten_matarazzo, dusty_henderson).
+actor(the_Witcher, henry_cavil, geralt_of_revia).
+actor(the_Witcher, anya_chalotra, yennifer_of_vengerberg).
+actor(the_Witcher, freya_allan, ciri).
+actor(pokemon_sun_and_moon, rica_matsumoto, satoshi).
+actor(pokemon_sun_and_moon, ikue_otani, pikachu).
+actor(charlie_and_the_chocolate_factory, johny_depp, willy_wonka).
+actor(charlie_and_the_chocolate_factory, freddie_highmore, charlie_bucket).
+actor(the_thing, kurt_russel, rj_macready).
+actor(the_thing, keith_david, childs).
+actor(back_to_the_future, michael_j_fox, marty_mcfly).
+actor(back_to_the_future, christopher_floyd, dr_emmet_brown).
+actor(ready_player_One, tye_sheridan, parzival/wade).
+actor(ready_player_One, olivia_cooke, art3mis/samantha).
+actor(deadpool, ryan_reynold, deadpool).
+actor(deadpool, morena_baccarin, vanessa).
+actor(jumanji, dwayne_johnson, spencer).
+actor(jumanji, kevin_hart, fridge).
+actor(jumanji, karen_gilan, martha).
+actor(jumanji, jack_black, bethany).
+actor(uncut_Gems, adam_sandler, howard_ratner).
+actor(uncut_Gems, idina_menzel, dinah_ratner).
+actor(ben10, tara_strong, ben_tennyson).
+actor(ben10, megan_moore, gwen_tennyson).
+actor(ben10, paul_eiding, max_tennyson).
+
+% :- use_module(netflix_facts,[]).
+
+:- dynamic 
+    mateage/2, 
+    releaseYear/1, 
+    matage/1, 
+    type/1, 
+    isFrom/1,
+    director/2,
+    actor/3.
+:- discontiguous actor/3.
+:- style_check(-singleton).
+ 
+% rules:_converted_into_code
+% Is this media a classic?
+isclassic(X) :-
+    releaseYear(X, Y),
+    Y=<2013.
+
+% Can kid watch this media?
+kidmedia(X) :-
+    matage(X, Y),
+    Y=<13.
+
+% A media for mature audience only
+maturemedia(X) :-
+    matage(X, Y),
+    Y>=18.
+
+% Is this media an anime?
+isanime(X) :-
+    type(X, Z),
+    Z==animation,
+    isfrom(X, Y),
+    Y==japan.
+
+% Similar family show
+similarfamilyshow(W, V) :-
+    kidmedia(W),
+    kidmedia(V),
+    type(W, X),
+    type(V, X),
+    W\=V.
+
+% Similar show for mature audiences
+similarmatureshow(W, V) :-
+    maturemedia(W),
+    maturemedia(V),
+    type(W, X),
+    type(V, X),
+    W\=V.
+
+% Director that direct media for mature audience
+maturedirector(V,W) :-
+    maturemedia(V),
+    director(V,W).
+
+% Director that directed a movie that this specific actor appear
+moviedirectorwithactor(D,A) :-
+    director(M,D),
+    actor(M, A, X).
+
+% Co-star of an actor in this movie.
+costar(X,Y) :-
+    actor(W,X,C),
+    actor(W,Y,D),
+    X \= Y.
+
+% Actor in a classic media.
+actorinclassicmedia(X,Y) :-
+    isclassic(Y),
+    actor(Y, X, C).
+
+% Specific medias with specific type that release in a specific year.
+maturetypemovieinYear(M,T,Y) :-
+    type(M,T),
+    maturemedia(X),
+    releaseYear(M,Y).
+    
+% New movie/series that came out this year.
+newmedia(X) :-
+    releaseYear(X, Y),
+    Y==2020.
+
+% Director that used to direct a classic movie.
+classicdirector(X,Y) :-
+    isclassic(Y),
+    director(Y,X).
+
+
+
